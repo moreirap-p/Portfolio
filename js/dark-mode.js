4cc81cb759eb838c39eec494484233f9d2eee9db
moreirap <paulo.moreira-pereira@etu.umontpellier.fr>
2025-02-16 01:27:59 +0100

    icon-unfix fix (wip)

--- js/dark-mode.js
+++ js/dark-mode.js
@@ -9,3 +9,14 @@ buttons.forEach(button => {
 
 //valeur "par défaut" lors du changement de page
 if (localStorage.getItem('dark') === 'enabled') document.body.classList.add('dark-mode')
+
+
+function switchIconToDark() {
+    const icon = document.getElementById('switch')
+    icon.src = "images/dark-mode.webp"
+}
+
+function switchIconToLight() {
+    const icon = document.getElementById('switch')
+    icon.src = "images/dark-mode-hover.webp"
+}
